d307124a551076f7cb3808174a447b32021472d3
yinzi <Myinzi123@gmail.com>
2020-01-03 15:25:52 +0800

    train AB, B(Resnet)

--- 2_tuning_cropper_model1.py
+++ 2_tuning_cropper_model1.py
@@ -14,17 +14,21 @@ from helper_funcs import F1Score, calc_centroid, affine_crop, affine_mapback
 from preprocess import ToPILImage, ToTensor, OrigPad, Resize
 from torch.utils.data import DataLoader
 from dataset import HelenDataset
-from model import Stage2Model, SelectNet
-from helper_funcs import affine_crop
+from model import Stage2Model, SelectNet, SelectNet_resnet
+import torchvision
+from helper_funcs import affine_crop, F1Score
 
 uuid = str(uid.uuid1())[0:8]
 print(uuid)
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=16, type=int, help="Batch size to use during training.")
 parser.add_argument("--display_freq", default=10, type=int, help="Display frequency")
-parser.add_argument("--lr", default=1e-5, type=float, help="Learning rate for optimizer1")
+parser.add_argument("--new", default=0, type=int, help="Use new select_net")
+parser.add_argument("--pretrainA", default=0, type=int, help="Load pretrainA")
+parser.add_argument("--pretrainB", default=0, type=int, help="Load pretrainB")
+parser.add_argument("--lr", default=1e-4, type=float, help="Learning rate for optimizer1")
 parser.add_argument("--lr2", default=0, type=float, help="Learning rate for optimizer2")
-parser.add_argument("--lr_s", default=1e-5, type=float, help="Learning rate for optimizer_select")
+parser.add_argument("--lr_s", default=1e-2, type=float, help="Learning rate for optimizer_select")
 parser.add_argument("--epochs", default=25, type=int, help="Number of epochs to train")
 parser.add_argument("--cuda", default=0, type=int, help="Choose GPU with cuda number")
 parser.add_argument("--eval_per_epoch", default=1, type=int, help="eval_per_epoch ")
@@ -46,19 +50,19 @@ transforms_list = {
     'train':
         transforms.Compose([
             ToTensor(),
-            Resize((128, 128)),
+            Resize((64, 64)),
             OrigPad()
         ]),
     'val':
         transforms.Compose([
             ToTensor(),
-            Resize((128, 128)),
+            Resize((64, 64)),
             OrigPad()
         ]),
     'test':
         transforms.Compose([
             ToTensor(),
-            Resize((128, 128)),
+            Resize((64, 64)),
             OrigPad()
         ])
 }
@@ -91,88 +95,106 @@ class TrainModel(TemplateModel):
         self.device = torch.device("cuda:%d" % args.cuda if torch.cuda.is_available() else "cpu")
 
         self.model = Stage1Model().to(self.device)
-        self.load_pretrained("model1")
+        if self.args.pretrainA:
+            self.load_pretrained("model1")
         self.model2 = Stage2Model().to(self.device)
         self.load_pretrained("model2")
-        self.select_net = SelectNet().to(self.device)
-        self.load_pretrained("select_net")
+        if self.args.new:
+            self.select_net = SelectNet_resnet().to(self.device)
+        else:
+            self.select_net = SelectNet().to(self.device)
+        if self.args.pretrainB:
+            self.load_pretrained("select_net")
         self.optimizer = optim.Adam(self.model.parameters(), self.args.lr)
-        self.optimizer2 = optim.Adam(self.model2.parameters(), self.args.lr2)
+        # self.optimizer2 = optim.Adam(self.model2.parameters(), self.args.lr2)
         self.optimizer_select = optim.Adam(self.select_net.parameters(), self.args.lr_s)
 
         self.criterion = nn.CrossEntropyLoss()
-        self.metric = nn.CrossEntropyLoss()
+        self.metric = F1Score(self.device)
         self.regress_loss = nn.SmoothL1Loss()
         self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=5, gamma=0.5)
-        self.scheduler2 = optim.lr_scheduler.StepLR(self.optimizer2, step_size=5, gamma=0.5)
+        # self.scheduler2 = optim.lr_scheduler.StepLR(self.optimizer2, step_size=5, gamma=0.5)
         self.scheduler3 = optim.lr_scheduler.StepLR(self.optimizer_select, step_size=5, gamma=0.5)
 
         self.train_loader = dataloader['train']
         self.eval_loader = dataloader['val']
-
-        self.ckpt_dir = "checkpoints_AB/%s" % uuid
+        if self.args.new:
+            self.ckpt_dir = "checkpoints_AB_new/%s" % uuid
+        else:
+            self.ckpt_dir = "checkpoints_AB/%s" % uuid
         self.display_freq = args.display_freq
-
         # call it to check all members have been intiated
         self.check_init()
 
     def train_loss(self, batch):
-        image, labels = batch['image'].to(self.device), batch['labels'].to(self.device)
-        orig = batch['orig'].to(self.device)
-        orig_label = batch['orig_label'].to(self.device)
+        image, label = batch['image'].to(self.device), batch['labels'].to(self.device)
+        orig, orig_label = batch['orig'].to(self.device), batch['orig_label'].to(self.device)
+        parts_mask_gt = batch['parts_mask_gt'].to(self.device)
         N, L, H, W = orig_label.shape
 
         stage1_pred = self.model(image)
-        assert stage1_pred.shape == (N, 9, 128, 128)
+        assert stage1_pred.shape == (N, 9, 64, 64)
 
         theta = self.select_net(F.softmax(stage1_pred, dim=1))
         assert theta.shape == (N, 6, 2, 3)
-
         assert orig_label.shape == (N, 9, 1024, 1024)
-        cens = calc_centroid(orig_label)
+        cens = torch.floor(calc_centroid(orig_label))
+
         assert cens.shape == (N, 9, 2)
-        points = torch.cat([cens[:, 1:6],
-                            cens[:, 6:9].mean(dim=1, keepdim=True)],
-                           dim=1)
+        points = torch.floor(torch.cat([cens[:, 1:6],
+                                        cens[:, 6:9].mean(dim=1, keepdim=True)],
+                                       dim=1))
         theta_label = torch.zeros((N, 6, 2, 3), device=self.device, requires_grad=False)
         for i in range(6):
             theta_label[:, i, 0, 0] = (81. - 1.) / (W - 1)
             theta_label[:, i, 0, 2] = -1. + (2. * points[:, i, 1]) / (W - 1)
             theta_label[:, i, 1, 1] = (81. - 1.) / (H - 1)
             theta_label[:, i, 1, 2] = -1. + (2. * points[:, i, 0]) / (H - 1)
-
         loss = self.regress_loss(theta, theta_label)
-
         return loss
 
     def eval_error(self):
         loss_list = []
+        step = 0
         for batch in self.eval_loader:
-            image, labels = batch['image'].to(self.device), batch['labels'].to(self.device)
-            orig = batch['orig'].to(self.device)
-            orig_label = batch['orig_label'].to(self.device)
+            step += 1
+            image, label = batch['image'].to(self.device), batch['labels'].to(self.device)
+            orig, orig_label = batch['orig'].to(self.device), batch['orig_label'].to(self.device)
             N, L, H, W = orig_label.shape
-
             stage1_pred = self.model(image)
             assert stage1_pred.shape == (N, 9, 128, 128)
 
             theta = self.select_net(F.softmax(stage1_pred, dim=1))
             assert theta.shape == (N, 6, 2, 3)
-
             assert orig_label.shape == (N, 9, 1024, 1024)
-            cens = calc_centroid(orig_label)
+            cens = torch.floor(calc_centroid(orig_label))
+
             assert cens.shape == (N, 9, 2)
-            points = torch.cat([cens[:, 1:6],
-                                cens[:, 6:9].mean(dim=1, keepdim=True)],
-                               dim=1)
+            points = torch.floor(torch.cat([cens[:, 1:6],
+                                            cens[:, 6:9].mean(dim=1, keepdim=True)],
+                                           dim=1))
             theta_label = torch.zeros((N, 6, 2, 3), device=self.device, requires_grad=False)
             for i in range(6):
                 theta_label[:, i, 0, 0] = (81. - 1.) / (W - 1)
                 theta_label[:, i, 0, 2] = -1. + (2. * points[:, i, 1]) / (W - 1)
                 theta_label[:, i, 1, 1] = (81. - 1.) / (H - 1)
                 theta_label[:, i, 1, 2] = -1. + (2. * points[:, i, 0]) / (H - 1)
+
             loss = self.regress_loss(theta, theta_label)
             loss_list.append(loss.item())
+
+        temp = []
+        for i in range(theta.shape[1]):
+            test = theta[:, i]
+            grid = F.affine_grid(theta=test, size=[N, 3, 81, 81], align_corners=True)
+            temp.append(F.grid_sample(input=orig, grid=grid, align_corners=True))
+        parts = torch.stack(temp, dim=1)
+        assert parts.shape == (N, 6, 3, 81, 81)
+        for i in range(6):
+            parts_grid = torchvision.utils.make_grid(
+                parts[:, i].detach().cpu())
+            self.writer.add_image('croped_parts_%s_%d' % (uuid, i), parts_grid, step)
+
         return np.mean(loss_list)
 
     def train(self):
@@ -192,7 +214,7 @@ class TrainModel(TemplateModel):
             self.optimizer.step()
 
             if self.step % self.display_freq == 0:
-                self.writer.add_scalar('loss', loss.item(), self.step)
+                self.writer.add_scalar('loss_%s' % uuid, torch.mean(loss).item(), self.step)
                 print('epoch {}\tstep {}\tloss {:.3}'.format(self.epoch, self.step, torch.mean(loss).item()))
 
     def eval(self):
@@ -205,7 +227,7 @@ class TrainModel(TemplateModel):
             self.best_error = error
             self.save_state(os.path.join(self.ckpt_dir, 'best.pth.tar'), False)
         self.save_state(os.path.join(self.ckpt_dir, '{}.pth.tar'.format(self.epoch)))
-        self.writer.add_scalar('error', error, self.epoch)
+        self.writer.add_scalar('error_%s' % uuid, error, self.epoch)
         print('epoch {}\terror {:.3}\tbest_error {:.3}'.format(self.epoch, error, self.best_error))
 
         return error
@@ -215,16 +237,16 @@ class TrainModel(TemplateModel):
 
         if isinstance(self.model, torch.nn.DataParallel):
             state['model1'] = self.model.module.state_dict()
-            state['model2'] = self.model2.module.state_dict()
+            # state['model2'] = self.model2.module.state_dict()
             state['select_net'] = self.select_net.module.state_dict()
         else:
             state['model1'] = self.model.state_dict()
-            state['model2'] = self.model2.state_dict()
+            # state['model2'] = self.model2.state_dict()
             state['select_net'] = self.select_net.state_dict()
 
         if optim:
             state['optimizer'] = self.optimizer.state_dict()
-            state['optimizer2'] = self.optimizer2.state_dict()
+            # state['optimizer2'] = self.optimizer2.state_dict()
             state['optimizer_select'] = self.optimizer_select.state_dict()
 
         state['step'] = self.step
@@ -233,15 +255,39 @@ class TrainModel(TemplateModel):
         torch.save(state, fname)
         print('save model at {}'.format(fname))
 
+    def load_pretrained(self, model):
+        path_model1 = os.path.join("/home/yinzi/data4/new_train/checkpoints_A/79648bf4", 'best.pth.tar')
+        # path_model1 = os.path.join("/home/yinzi/data4/new_train/checkpoints_A/61ad8a38", 'best.pth.tar')
+        # path_select = os.path.join("/home/yinzi/data4/new_train/checkpoints_B/9a95687c", 'best.pth.tar')
+        #checkpoints_B_new/0168b2ce resnet结构
+        path_select = os.path.join("/home/yinzi/data4/new_train/checkpoints_B_new/0168b2ce", 'best.pth.tar')
+
+        path_model2 = os.path.join("/home/yinzi/data4/new_train/checkpoints_C/396e4702", "best.pth.tar")
+        if model == 'model1':
+            fname = path_model1
+            state = torch.load(fname, map_location=self.device)
+            self.model.load_state_dict(state['model1'])
+            print("load from" + fname)
+        elif model == 'model2':
+            fname = path_model2
+            state = torch.load(fname, map_location=self.device)
+            self.model2.load_state_dict(state['model2'])
+            print("load from" + fname)
+        elif model == 'select_net':
+            fname = path_select
+            state = torch.load(fname, map_location=self.device)
+            self.select_net.load_state_dict(state['select_net'])
+            print("load from" + fname)
+
 
 def start_train():
     train = TrainModel(args)
 
     for epoch in range(args.epochs):
         train.train()
-        train.scheduler.step()
-        # train.scheduler2.step()
-        train.scheduler3.step()
+        train.scheduler.step(epoch)
+        # train.scheduler2.step(epoch)
+        train.scheduler3.step(epoch)
         if (epoch + 1) % args.eval_per_epoch == 0:
             train.eval()
 
